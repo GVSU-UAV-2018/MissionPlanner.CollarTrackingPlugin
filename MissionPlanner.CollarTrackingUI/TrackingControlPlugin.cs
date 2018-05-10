f1f0d597d6473bb493a3276c228cb0f334e00afd
willisoa <willisoa@mail.gvsu.edu>
2018-05-10 14:21:34 -0400

    Adding all files for the MissionPlanner Collar Tracking User Interface Plug-In

---
+++ MissionPlanner.CollarTrackingUI/TrackingControlPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Forms;
+
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
+using MissionPlanner;
+using MissionPlanner.Utilities;
+
+namespace MissionPlanner.CollarTrackingUI
+{
+    public class TrackingControlPlugin : MissionPlanner.Plugin.Plugin
+    {
+        Form f = new Form();
+
+        public override string Name => "Collar Tracking Interface Plugin";
+
+        public override string Version => "1.0";
+
+        public override string Author => "2018 UAV Senior Project Team";
+
+        public override bool Init()
+        {
+            f.Text = "Test";
+            f.Size = new System.Drawing.Size(500, 500);
+            CollarTrackingControl ctc = new CollarTrackingControl();
+            f.Controls.Add(ctc);
+            ctc.Dock = DockStyle.Fill;
+
+            f.Show();
+            return true;
+        }
+
+        public override bool Loaded()
+        {
+            return true;
+        }
+
+        public override bool Exit()
+        {
+            f.Hide();
+            return true;
+        }
+
+        public override bool Loop()
+        {
+            return true;
+        }
+    }
+}
---
+++ MissionPlanner.CollarTrackingUI/UserControls/CollarTrackingControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MissionPlanner.CollarTrackingUI
+{
+    public partial class CollarTrackingControl: UserControl
+    {
+        public CollarTrackingControl()
+        {
+            InitializeComponent();
+        }
+
+        private void CollarTrackingSetFrequencyButton_Click(object sender, EventArgs e)
+        {
+            this.CollarTrackingSetFrequencyButton.Text = "Collar Frequency: " + 
+                CollarTrackingFrequencyTextBox.Text + " MHz";
+        }
+
+        private void CollarTrackingStartScanButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ColarrTrackingCancelScanButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
